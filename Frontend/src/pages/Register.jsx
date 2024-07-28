a8f073e03c60cb95c81ac2ae7ced9bf4a878206a
Vikash Kumar Singh <kumarrohit93334@gmail.com>
2024-07-28 23:54:27 +0530

    add client side ui and refister component

---
+++ Frontend/src/pages/Register.jsx
@@ -0,0 +1,55 @@
+export const Register = () => {
+  //  Help me reach 1 Million subs 👉 https://youtube.com/thapatechnical
+
+  return (
+    <>
+      <section>
+        <main>
+          <div className="section-registration">
+            <div className="container grid grid-two-cols">
+              <div className="registration-image reg-img">
+                <img
+                  src="/images/register.png"
+                  alt="a nurse with a cute look"
+                  width="400"
+                  height="500"
+                />
+              </div>
+              {/* our main registration code  */}
+              <div className="registration-form">
+                <h1 className="main-heading mb-3">registration form</h1>
+                <br />
+                <form>
+                  <div>
+                    <label htmlFor="username">username</label>
+                    <input type="text" name="username" placeholder="username" />
+                  </div>
+                  <div>
+                    <label htmlFor="email">email</label>
+                    <input type="text" name="email" placeholder="email" />
+                  </div>
+                  <div>
+                    <label htmlFor="phone">phone</label>
+                    <input type="number" name="phone" />
+                  </div>
+                  <div>
+                    <label htmlFor="password">password</label>
+                    <input
+                      type="password"
+                      name="password"
+                      placeholder="password"
+                    />
+                  </div>
+                  <br />
+                  <button type="submit" className="btn btn-submit">
+                    Register Now
+                  </button>
+                </form>
+              </div>
+            </div>
+          </div>
+        </main>
+      </section>
+    </>
+  );
+};
